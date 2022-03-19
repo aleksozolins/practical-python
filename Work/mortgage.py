fe02759233f37b870442e4eeab187391033e7f85
aleksozolins <aleks@aleksozolins.com>
2022-03-19 17:12:22 -0400

    Extra payments solved

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,14 +5,18 @@
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
-ex_payment_amt = 1000
-ex_payment = 0
 total_paid = 0.0
-month = 0
+month = 1
 
-while principal > 0 and ex_payment < 12:
-    principal = principal * (1+rate/12) - payment - ex_payment_amt * ex_payment
-    total_paid = total_paid + payment + ex_payment
+while month < 13:
+    principal = principal * (1+rate/12) - payment - 1000
+    total_paid = total_paid + payment + 1000
+    print (month, 'extra', total_paid)
+    month = month + 1
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
     print (month, total_paid)
     month = month + 1
 
