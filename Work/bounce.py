23dbd803d3398759d385486341bc9ae27284710d
Aleks Ozolins <aleks@ozolins.xyz>
2025-06-08 10:16:39 -0400

    Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # Meters
+drop = 1 # Which time it is dropped
+
+while drop <= 10:
+    height = height * .6
+    print(drop, round(height, 4))
+    drop += 1
